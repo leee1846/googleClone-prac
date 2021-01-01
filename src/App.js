d29503905a147638f9e1ec6765007bdf566a1343
leee1846 <leee1846@gmail.com>
2021-01-01 19:45:23 +0900

    feature: header,searchInput 완성

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import Home from "./Pages/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Search from "./Pages/Search";
 
 function App() {
   return (
     <>
-      <div>hh</div>
-
-      <Home />
+      <Router>
+        <Switch>
+          <Route path='/Search' component={Search}>
+            xx
+          </Route>
+          <Route exact path='/' component={Home}></Route>
+        </Switch>
+      </Router>
     </>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,35 @@
 import React from "react";
 import "./Home.css";
-import MicIcon from "@material-ui/icons/Mic";
+import { Link } from "react-router-dom";
+import AppsIcon from "@material-ui/icons/Apps";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import Search from "./Search";
 
 function Home() {
   return (
     <div className='home'>
-      <div className='home'>SS</div>
-
       <header className='home__header'>
         <div className='home__headerLeft'>
-          <MicIcon />
+          <Link to='/about'>About</Link>
+          <Link to='/store'>Store</Link>
         </div>
-        <div className='home__headerRight'></div>
+        <div className='home__headerRight'>
+          <Link to='/gmail'>Gmail</Link>
+          <Link to='/images'>Images</Link>
+          <AppsIcon className='header__icons icon' />
+          <AccountCircleIcon className='header__icon icon' />
+        </div>
       </header>
 
-      <div className='home__body'></div>
+      <div className='home__body'>
+        <img
+          src='https://cdn.vox-cdn.com/thumbor/Ous3VQj1sn4tvb3H13rIu8eGoZs=/0x0:2012x1341/1400x788/filters:focal(0x0:2012x1341):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/47070706/google2.0.0.jpg'
+          alt=''
+        />
+        <div className='home__inputContainer'>
+          <Search />
+        </div>
+      </div>
     </div>
   );
 }
