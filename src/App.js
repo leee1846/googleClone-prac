750f6fadd5a1ba1b48f06449ba63c1252b01d5a7
leee1846 <leee1846@gmail.com>
2021-01-01 18:42:41 +0900

    feature: 폴더트리 구성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import "./App.css";
+import Home from "./Pages/Home";
+
 function App() {
-  return <div>la</div>;
+  return (
+    <>
+      <div>hh</div>
+
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className='home'>SS</div>
+      <div className='home__header'></div>
+      <div className='home__body'></div>
+    </>
+  );
+}
+
+export default Home;
