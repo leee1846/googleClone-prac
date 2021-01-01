bb29944d9c9f2879218d6086308e476bc35ffd1d
leee1846 <leee1846@gmail.com>
2021-01-01 18:48:01 +0900

    feature: materialUi 설치

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
 import "./Home.css";
+import MicIcon from "@material-ui/icons/Mic";
 
 function Home() {
   return (
-    <>
+    <div className='home'>
       <div className='home'>SS</div>
-      <div className='home__header'></div>
+
+      <header className='home__header'>
+        <div className='home__headerLeft'>
+          <MicIcon />
+        </div>
+        <div className='home__headerRight'></div>
+      </header>
+
       <div className='home__body'></div>
-    </>
+    </div>
   );
 }
 
